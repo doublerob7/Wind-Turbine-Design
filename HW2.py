bfd89fc248a9f11fdefd51c857a85cf8641cb644
doublerob7 <doublerob7@gmail.com>
2016-11-29 15:19:47 -0800

    created initial BladeSection and Blade classes

--- HW2.py
+++ HW2.py
@@ -9,60 +9,153 @@ Homework #2
 """
 
 ''' Setup Litany '''
-import scipy as s
 import matplotlib.pyplot as plt
-
-import functions as func
-
-
-''' 3.(a) Determine the pdf for the wind velocity for each month, as well as a 
-    year. Plot Jan, Apr, July, Oct and a yearly total of all 10 years. '''
-
-bin_width = 2
-
-jan_data = func.WindStats('data/Laramie2005_2015.dat', year='all', month='Jan', bin_width=bin_width)
-apr_data = func.WindStats('data/Laramie2005_2015.dat', year='all', month='Apr', bin_width=bin_width)
-jul_data = func.WindStats('data/Laramie2005_2015.dat', year='all', month='Jul', bin_width=bin_width)
-oct_data = func.WindStats('data/Laramie2005_2015.dat', year='all', month='Oct', bin_width=bin_width)
-all_data = func.WindStats('data/Laramie2005_2015.dat', year='all', month='all', bin_width=bin_width)
-
-bins = range(0, 40, bin_width)
-
-plt.hist(jan_data.speed, bins, normed=True, histtype='step', color='blue', label='Jan')
-plt.hist(apr_data.speed, bins, normed=True, histtype='step', color='red', label='Apr')
-plt.hist(jul_data.speed, bins, normed=True, histtype='step', color='orange', label='Jul')
-plt.hist(oct_data.speed, bins, normed=True, histtype='step', color='green', label='Oct')
-plt.hist(all_data.speed, bins, normed=True, histtype='step', color='black', label='All')
-
-plt.title('Normalized distribution of wind speeds')
-plt.grid()
-plt.legend()
+from HW1 import WindDataReader
 
 
-''' 3.(b) Using the pdf, determine the mean wind velocity and the wind variance
-    for all periods from 3.(a). Provide a table. '''
+class WindStats(WindDataReader):
+    """ Extends WindDataReader class with analysis methods for wind data """
 
-print("Month |", "Standard Mean |", "PDF Mean |", "Variance")
+    def __init__(self, filename, year, month, bin_width, debug=False):
+        super().__init__(filename, year, month, debug)
+        self._bin_width = bin_width
+        self._pdf = None
+        self._pdf_mean = None
+        self._mean = None
+        self._variance = None
+        self._weibull_params = None
+        self._rayleigh_params = None
+        self.bin_centers = []
 
-print('{:^6}'.format(jan_data.month), '{:^15.2f}'.format(jan_data.mean), '{:^10.2f}'.format(jan_data.pdf_mean), '{:^10.2f}'.format(jan_data.variance))
-print('{:^6}'.format(apr_data.month), '{:^15.2f}'.format(apr_data.mean), '{:^10.2f}'.format(apr_data.pdf_mean), '{:^10.2f}'.format(apr_data.variance))
-print('{:^6}'.format(jul_data.month), '{:^15.2f}'.format(jul_data.mean), '{:^10.2f}'.format(jul_data.pdf_mean), '{:^10.2f}'.format(jul_data.variance))
-print('{:^6}'.format(oct_data.month), '{:^15.2f}'.format(oct_data.mean), '{:^10.2f}'.format(oct_data.pdf_mean), '{:^10.2f}'.format(oct_data.variance))
-print('{:^6}'.format(all_data.month), '{:^15.2f}'.format(all_data.mean), '{:^10.2f}'.format(all_data.pdf_mean), '{:^10.2f}'.format(all_data.variance))
+    @property
+    def bin_width(self):
+        return self._bin_width
+
+    @bin_width.setter
+    def bin_width(self, value):
+        if self._bin_width is None:
+            self._bin_width = int(value)
+
+    @property
+    def variance(self):
+        if self._variance is None:
+            from scipy import var
+            self._variance = var(self.speed)
+        return self._variance
+
+    @property
+    def mean(self):
+        if self._mean is None:
+            from scipy import mean
+            self._mean = mean(self.speed)
+        return self._mean
+
+    @property
+    def pdf(self):
+        if self._pdf is None:
+            from scipy import histogram
+            self._pdf = histogram(self.speed, range(0, 50, self.bin_width), density=True)
+        return self._pdf
+
+    @property
+    def pdf_mean(self):
+        if self._pdf_mean is None:
+            from scipy import average
+            self.calc_bin_centers()
+            try:
+                self._pdf_mean = average(self.bin_centers, weights=self.pdf[0])
+            except:
+                print(self.bin_centers, len(self.bin_centers))
+                print(self.pdf[0], len(self.pdf[0]))
+        return self._pdf_mean
+
+    @property
+    def weibull_params(self):
+        if self._weibull_params is None:
+            from scipy.stats import exponweib
+            self._weibull_params = exponweib.fit(self.speed, floc=0)
+        return self._weibull_params
+
+    @property
+    def rayleigh_params(self):
+        if self._rayleigh_params is None:
+            from scipy.stats import rayleigh
+            self._rayleigh_params = rayleigh.fit(self.speed, floc=0)
+        return self._rayleigh_params
+
+    def calc_bin_centers(self):
+        center_range = range(0, len(self.pdf[0]))
+        for i in center_range:
+            next_edge = self.pdf[1][i + 1]
+            this_edge = self.pdf[1][i]
+            self.bin_centers.append((next_edge - this_edge) / 2 + this_edge)
+
+    def plot_pdf_fit(self, label=None):
+        import matplotlib.pyplot as plt
+        from scipy.stats import exponweib, rayleigh
+        from scipy import linspace, diff
+
+        plt.bar(self.pdf[1][:len(self.pdf[0])], self.pdf[0], width=diff(self.pdf[1]), label=label, alpha=0.5, color='k')
+
+        x = linspace(0, 50, 1000)
+
+        plt.plot(x, exponweib.pdf(x, a=self.weibull_params[0], c=self.weibull_params[1], scale=self.weibull_params[3]), 'b--', label='Exponential Weibull pdf')
+        plt.plot(x, rayleigh.pdf(x, scale=self.rayleigh_params[1]), 'r--', label='Rayleigh pdf')
+
+        plt.title('Normalized distribution of wind speeds')
+        plt.grid()
+        plt.legend()
+
+if __name__ == "__main__":
+
+    ''' 3.(a) Determine the pdf for the wind velocity for each month, as well as a
+        year. Plot Jan, Apr, July, Oct and a yearly total of all 10 years. '''
+
+    bin_width = 2
+
+    jan_data = WindStats('data/Laramie2005_2015.dat', year='all', month='Jan', bin_width=bin_width)
+    apr_data = WindStats('data/Laramie2005_2015.dat', year='all', month='Apr', bin_width=bin_width)
+    jul_data = WindStats('data/Laramie2005_2015.dat', year='all', month='Jul', bin_width=bin_width)
+    oct_data = WindStats('data/Laramie2005_2015.dat', year='all', month='Oct', bin_width=bin_width)
+    all_data = WindStats('data/Laramie2005_2015.dat', year='all', month='all', bin_width=bin_width)
+
+    bins = range(0, 40, bin_width)
+
+    plt.hist(jan_data.speed, bins, normed=True, histtype='step', color='blue', label='Jan')
+    plt.hist(apr_data.speed, bins, normed=True, histtype='step', color='red', label='Apr')
+    plt.hist(jul_data.speed, bins, normed=True, histtype='step', color='orange', label='Jul')
+    plt.hist(oct_data.speed, bins, normed=True, histtype='step', color='green', label='Oct')
+    plt.hist(all_data.speed, bins, normed=True, histtype='step', color='black', label='All')
+
+    plt.title('Normalized distribution of wind speeds')
+    plt.grid()
+    plt.legend()
 
 
-''' 3.(c) Overlay a Weibull and Rayleigh distribution on the pdfs and determine
-    a best fit for each. '''
+    ''' 3.(b) Using the pdf, determine the mean wind velocity and the wind variance
+        for all periods from 3.(a). Provide a table. '''
 
-plt.figure()
-jan_data.plot_pdf_fit("Jan")
-plt.figure()
-apr_data.plot_pdf_fit("Apr")
-plt.figure()
-jul_data.plot_pdf_fit("Jul")
-plt.figure()
-oct_data.plot_pdf_fit("Oct")
-plt.figure()
-all_data.plot_pdf_fit("All")
+    print("Month |", "Standard Mean |", "PDF Mean |", "Variance")
 
-plt.show()
+    print('{:^6}'.format(jan_data.month), '{:^15.2f}'.format(jan_data.mean), '{:^10.2f}'.format(jan_data.pdf_mean), '{:^10.2f}'.format(jan_data.variance))
+    print('{:^6}'.format(apr_data.month), '{:^15.2f}'.format(apr_data.mean), '{:^10.2f}'.format(apr_data.pdf_mean), '{:^10.2f}'.format(apr_data.variance))
+    print('{:^6}'.format(jul_data.month), '{:^15.2f}'.format(jul_data.mean), '{:^10.2f}'.format(jul_data.pdf_mean), '{:^10.2f}'.format(jul_data.variance))
+    print('{:^6}'.format(oct_data.month), '{:^15.2f}'.format(oct_data.mean), '{:^10.2f}'.format(oct_data.pdf_mean), '{:^10.2f}'.format(oct_data.variance))
+    print('{:^6}'.format(all_data.month), '{:^15.2f}'.format(all_data.mean), '{:^10.2f}'.format(all_data.pdf_mean), '{:^10.2f}'.format(all_data.variance))
+
+
+    ''' 3.(c) Overlay a Weibull and Rayleigh distribution on the pdfs and determine
+        a best fit for each. '''
+
+    plt.figure()
+    jan_data.plot_pdf_fit("Jan")
+    plt.figure()
+    apr_data.plot_pdf_fit("Apr")
+    plt.figure()
+    jul_data.plot_pdf_fit("Jul")
+    plt.figure()
+    oct_data.plot_pdf_fit("Oct")
+    plt.figure()
+    all_data.plot_pdf_fit("All")
+
+    plt.show()
--- HW3.py
+++ HW3.py
@@ -14,193 +14,218 @@ import numpy as np
 import matplotlib.pyplot as plot
 import functions as func
 
-# clear_all()
 
-""" Given Parameters """
+class BladeSection:
+    """
 
-r = [4,6,8,10,12,14,16,18,20] # m
-chord = [1.5,1.4,1.3,1.2,1.0,0.8,0.6,0.4,0.2] # m
-theta_p = [26,16,10,5.7,3.7,2.5,2.0,1.5,1.0] # deg
+    """
 
-rho = 1.23 # kg/m^3
-ux1 = 12 # m/s
-
-blade_min_r = 3 # m
-R = 21 # m
-RPM = 30
-omega = (np.float(RPM)/60) * 2 * np.pi # rads/s
-N_blades = 3
-N_sections = 9
-
-alpha = np.zeros(9)
-alpha_corr = np.zeros(9)
-alpha_corr_deg = np.zeros(9)
-C_N = np.zeros(9)
-C_N_corr = np.zeros(9)
-C_T = np.zeros(9)
-C_T_corr = np.zeros(9)
-F_N = np.zeros(9)
-F_N_corr = np.zeros(9)
-F_T = np.zeros(9)
-F_T_corr = np.zeros(9) 
-
-filename = ['data\DU97W300AoAClCd.csv','data\DU91W250AoAClCd.csv','data\DU95W180AoAClCd.csv']
-
-# step 1: Guess the induction factors a and a'
-    
-a = 0.1 * np.ones(9)
-a_corr = np.zeros(9)
-a_prime = 0.1 * np.ones(9)
-a_prime_corr = np.zeros(9)
-
-for i in range(0,8+1):
-
-    tol = 1
-    itera = 0
-    
-    if i < 3:
-        currentfile = filename[0]
-    elif i < 6:
-        currentfile = filename[1]
-    else:
-        currentfile = filename[2]
-    
-    while tol > 10e-6:
-
-        # step 2: Compute flow angle phi
-        
-        tanarg = ( (1-a[i]) * ux1 ) / ( (1+a_prime[i]) * omega * r[i] )
-        phi = np.arctan(tanarg)
-        phi_deg = np.degrees(phi)
-        
-        # step 3: compute local angle of attack alpha
-        
-        alpha[i] = phi - np.radians(theta_p[i])
-        alpha_deg = np.degrees(alpha)
-        
-        # step 4: Determine C_L(alpha) and C_D(alpha) using airfoil properties
-        
-        CLCD = func.get_CLCD(currentfile,alpha_deg[i])
-        Cl = CLCD[0]
-        Cd = CLCD[1]
-        
-        # step 5: Determine C_N and C_T from C_L and C_D
-        
-        C_N[i] = Cl * np.cos(phi) + Cd * np.sin(phi)
-        C_T[i] = Cl * np.sin(phi) - Cd * np.cos(phi)
-        
-        # step 6: Calculate a and a'
-        
-        s = np.sin(phi)
-        c = np.cos(phi)
-        
-        sigma = chord[i] * N_blades / (2*np.pi*r[i])
-        
-        lasta = a[i]
-        a[i] = (1+ (4*(s**2)/(sigma*C_N[i]) ))**(-1)
-        
-        lasta_prime = a_prime[i]
-        a_prime[i] = ((4*s*c)/(sigma*C_T[i])-1)**-1
-        
-        # step 7: Calculate F_N and F_T from C_N and C_T
-        
-        w = (ux1 * (1-a[i])) / np.sin(phi) 
-        
-        F_N[i] = C_N[i] * .5 * rho * chord[i] * (w**2)
-        F_T[i] = C_T[i] * .5 * rho * chord[i] * (w**2)
-        
-        tol = abs(a[i] - lasta)/lasta
-        itera += 1
-        
-    print('Sec:',i+1, 'Iter:', itera, ' a: {0:.6}'.format(a[i]),' lasta: {0:.6}'.format(lasta),' alpha: {0:.6}'.format(alpha_deg[i]), ' Tolerance:',tol)
-        
-    # Prandtl Tip correction calcs
-    f = N_blades/2*(R-r[i])/(r[i]*np.sin(phi))
-    F = 2/np.pi * np.arccos(np.exp(-f))
-    
-    a_corr[i] = (1+(4*F*s**2)/(sigma*C_N[i]))**-1
-    a_prime_corr[i] = ((4*F*s*c)/(sigma*C_T[i])-1)**-1
-    
-    phi_corr = np.arctan(((1-a_corr[i])*ux1)/((1+a_prime_corr[i])*omega*r[i]))
-    
-    alpha_corr[i] = phi_corr - np.radians(theta_p[i])
-    alpha_corr_deg[i] = np.degrees(alpha_corr[i])
-    
-    CLCD_corr = func.get_CLCD(currentfile, alpha_corr_deg[i])
-    Cl_corr = CLCD_corr[0]
-    Cd_corr = CLCD_corr[1]
-    
-    C_N_corr[i] = Cl_corr * np.cos(phi_corr) + Cd_corr * np.sin(phi_corr)
-    C_T_corr[i] = Cl_corr * np.sin(phi_corr) - Cd_corr * np.cos(phi_corr)
-    
-    F_N_corr[i] = C_N_corr[i] * .5 * rho * chord[i] * (w**2)
-    F_T_corr[i] = C_T_corr[i] * .5 * rho * chord[i] * (w**2)
-    
-    #print '\n',f,F,'\n'
-
-print('\n')
-
-""" (a) Plot the local angle of attack, alpha, in degrees as a function of r.
-Plot with and without the Prantl tip correction """
-
-alpha = np.degrees(alpha)
-alpha_corr = np.degrees(alpha_corr)
-
-plot.figure(1)
-plot.plot(r,alpha, 'k-', label="AoA without Prandtl correction")
-plot.plot(r,alpha_corr, 'k--', label="AoA with Prandtl correction")
-plot.legend(loc='lower left')
-plot.title('Angle of attack over the length of the blade')
-plot.xlabel('Radius: hub to tip midpoints [m]')
-plot.ylabel('AoA, alpha [deg]')
-plot.grid()
+    def __init__(self, radius, sec_length, chord, twist, filename):
+        self.radius = radius
+        self.sec_length = sec_length
+        self.chord = chord
+        self.twist = twist
+        self.filename = filename
 
 
+class Blade:
+    """
 
-""" (b) Plot the local Normal and Tangential force Coefficients as a function 
-of r. Plot with and without the Prantl tip correction. """
-
-plot.figure(2)
-plot.plot(r,C_N_corr, 'k-', label="Normal Coef with Prandtl correction")
-plot.plot(r,C_T_corr, 'k--', label="Tangential Coef with Prandtl correction")
-plot.legend(loc='upper left')
-plot.title('Force Coefficients along blade length')
-plot.xlabel('Radius: hub to tip midpoints [m]')
-plot.ylabel('Force coefficient')
-plot.grid()
-
-plot.figure(3)
-plot.plot(r,F_N_corr, 'k-', label="Normal Force with Prandtl correction")
-plot.plot(r,F_T_corr, 'k--', label="Tangential Force with Prandtl correction")
-plot.legend(loc='upper left')
-plot.title('Force along blade length')
-plot.xlabel('Radius: hub to tip midpoints [m]')
-plot.ylabel('Force [N]')
-plot.grid()
+    """
+    def __init__(self, size, sections, section_chord, blade_twist, ):
+        self.size = size
+        self.sections = sections
+        self.chord = section_chord
+        self.twist = blade_twist
 
 
-""" (c) Determine total Thrust, Torque and Power experienced by the blades 
-using the Prantl correction. """
+if __name__ == "__main__":
 
-# Thrust: the total normal force acting along the entire length of all 3 blades
-sectionlength = r[1] - r[0]
-thrust = 0
-for i, section in enumerate(r):
-    thrust += F_N_corr[i] * sectionlength
-        
-thrust *= N_blades
-print('Total Thrust:  {0:.2f} kN'.format(thrust*10**-3))
+    """ Given Parameters """
 
-# Torque: sum(F_T[i] x r[i]) the sum of each tangential force times it's radius
-torque = 0
-for i, section in enumerate(r):
-    torque += F_T_corr[i]*r[i] * sectionlength
-        
-torque *= N_blades
-print('Total Torque:  {0:.2f} kN m'.format(torque*10**-3))
+    r = [4,6,8,10,12,14,16,18,20] # m
+    chord = [1.5,1.4,1.3,1.2,1.0,0.8,0.6,0.4,0.2] # m
+    theta_p = [26,16,10,5.7,3.7,2.5,2.0,1.5,1.0] # deg
 
-# Power: torque x angular velocity
-power = torque * omega
-print('Total Power:  {0:.2f} kW'.format(power*10**-3))
+    rho = 1.23 # kg/m^3
+    ux1 = 12 # m/s
 
-plot.show()
+    blade_min_r = 3 # m
+    R = 21 # m
+    RPM = 30
+    omega = (np.float(RPM)/60) * 2 * np.pi # rads/s
+    N_blades = 3
+    N_sections = 9
+
+    alpha = np.zeros(9)
+    alpha_corr = np.zeros(9)
+    alpha_corr_deg = np.zeros(9)
+    C_N = np.zeros(9)
+    C_N_corr = np.zeros(9)
+    C_T = np.zeros(9)
+    C_T_corr = np.zeros(9)
+    F_N = np.zeros(9)
+    F_N_corr = np.zeros(9)
+    F_T = np.zeros(9)
+    F_T_corr = np.zeros(9)
+
+    filename = ['data\DU97W300AoAClCd.csv','data\DU91W250AoAClCd.csv','data\DU95W180AoAClCd.csv']
+
+    # step 1: Guess the induction factors a and a'
+
+    a = 0.1 * np.ones(9)
+    a_corr = np.zeros(9)
+    a_prime = 0.1 * np.ones(9)
+    a_prime_corr = np.zeros(9)
+
+    for i in range(0,8+1):
+
+        tol = 1
+        itera = 0
+
+        if i < 3:
+            currentfile = filename[0]
+        elif i < 6:
+            currentfile = filename[1]
+        else:
+            currentfile = filename[2]
+
+        while tol > 10e-6:
+
+            # step 2: Compute flow angle phi
+
+            tanarg = ( (1-a[i]) * ux1 ) / ( (1+a_prime[i]) * omega * r[i] )
+            phi = np.arctan(tanarg)
+            phi_deg = np.degrees(phi)
+
+            # step 3: compute local angle of attack alpha
+
+            alpha[i] = phi - np.radians(theta_p[i])
+            alpha_deg = np.degrees(alpha)
+
+            # step 4: Determine C_L(alpha) and C_D(alpha) using airfoil properties
+
+            CLCD = func.get_CLCD(currentfile,alpha_deg[i])
+            Cl = CLCD[0]
+            Cd = CLCD[1]
+
+            # step 5: Determine C_N and C_T from C_L and C_D
+
+            C_N[i] = Cl * np.cos(phi) + Cd * np.sin(phi)
+            C_T[i] = Cl * np.sin(phi) - Cd * np.cos(phi)
+
+            # step 6: Calculate a and a'
+
+            s = np.sin(phi)
+            c = np.cos(phi)
+
+            sigma = chord[i] * N_blades / (2*np.pi*r[i])
+
+            lasta = a[i]
+            a[i] = (1+ (4*(s**2)/(sigma*C_N[i]) ))**(-1)
+
+            lasta_prime = a_prime[i]
+            a_prime[i] = ((4*s*c)/(sigma*C_T[i])-1)**-1
+
+            # step 7: Calculate F_N and F_T from C_N and C_T
+
+            w = (ux1 * (1-a[i])) / np.sin(phi)
+
+            F_N[i] = C_N[i] * .5 * rho * chord[i] * (w**2)
+            F_T[i] = C_T[i] * .5 * rho * chord[i] * (w**2)
+
+            tol = abs(a[i] - lasta)/lasta
+            itera += 1
+
+        print('Sec:',i+1, 'Iter:', itera, ' a: {0:.6}'.format(a[i]),' lasta: {0:.6}'.format(lasta),' alpha: {0:.6}'.format(alpha_deg[i]), ' Tolerance:',tol)
+
+        # Prandtl Tip correction calcs
+        f = N_blades/2*(R-r[i])/(r[i]*np.sin(phi))
+        F = 2/np.pi * np.arccos(np.exp(-f))
+
+        a_corr[i] = (1+(4*F*s**2)/(sigma*C_N[i]))**-1
+        a_prime_corr[i] = ((4*F*s*c)/(sigma*C_T[i])-1)**-1
+
+        phi_corr = np.arctan(((1-a_corr[i])*ux1)/((1+a_prime_corr[i])*omega*r[i]))
+
+        alpha_corr[i] = phi_corr - np.radians(theta_p[i])
+        alpha_corr_deg[i] = np.degrees(alpha_corr[i])
+
+        CLCD_corr = func.get_CLCD(currentfile, alpha_corr_deg[i])
+        Cl_corr = CLCD_corr[0]
+        Cd_corr = CLCD_corr[1]
+
+        C_N_corr[i] = Cl_corr * np.cos(phi_corr) + Cd_corr * np.sin(phi_corr)
+        C_T_corr[i] = Cl_corr * np.sin(phi_corr) - Cd_corr * np.cos(phi_corr)
+
+        F_N_corr[i] = C_N_corr[i] * .5 * rho * chord[i] * (w**2)
+        F_T_corr[i] = C_T_corr[i] * .5 * rho * chord[i] * (w**2)
+
+        #print '\n',f,F,'\n'
+
+    print('\n')
+
+    """ (a) Plot the local angle of attack, alpha, in degrees as a function of r.
+    Plot with and without the Prantl tip correction """
+
+    alpha = np.degrees(alpha)
+    alpha_corr = np.degrees(alpha_corr)
+
+    plot.figure(1)
+    plot.plot(r,alpha, 'k-', label="AoA without Prandtl correction")
+    plot.plot(r,alpha_corr, 'k--', label="AoA with Prandtl correction")
+    plot.legend(loc='lower left')
+    plot.title('Angle of attack over the length of the blade')
+    plot.xlabel('Radius: hub to tip midpoints [m]')
+    plot.ylabel('AoA, alpha [deg]')
+    plot.grid()
+
+
+
+    """ (b) Plot the local Normal and Tangential force Coefficients as a function
+    of r. Plot with and without the Prantl tip correction. """
+
+    plot.figure(2)
+    plot.plot(r,C_N_corr, 'k-', label="Normal Coef with Prandtl correction")
+    plot.plot(r,C_T_corr, 'k--', label="Tangential Coef with Prandtl correction")
+    plot.legend(loc='upper left')
+    plot.title('Force Coefficients along blade length')
+    plot.xlabel('Radius: hub to tip midpoints [m]')
+    plot.ylabel('Force coefficient')
+    plot.grid()
+
+    plot.figure(3)
+    plot.plot(r,F_N_corr, 'k-', label="Normal Force with Prandtl correction")
+    plot.plot(r,F_T_corr, 'k--', label="Tangential Force with Prandtl correction")
+    plot.legend(loc='upper left')
+    plot.title('Force along blade length')
+    plot.xlabel('Radius: hub to tip midpoints [m]')
+    plot.ylabel('Force [N]')
+    plot.grid()
+
+
+    """ (c) Determine total Thrust, Torque and Power experienced by the blades
+    using the Prantl correction. """
+
+    # Thrust: the total normal force acting along the entire length of all 3 blades
+    sectionlength = r[1] - r[0]
+    thrust = 0
+    for i, section in enumerate(r):
+        thrust += F_N_corr[i] * sectionlength
+
+    thrust *= N_blades
+    print('Total Thrust:  {0:.2f} kN'.format(thrust*10**-3))
+
+    # Torque: sum(F_T[i] x r[i]) the sum of each tangential force times it's radius
+    torque = 0
+    for i, section in enumerate(r):
+        torque += F_T_corr[i]*r[i] * sectionlength
+
+    torque *= N_blades
+    print('Total Torque:  {0:.2f} kN m'.format(torque*10**-3))
+
+    # Power: torque x angular velocity
+    power = torque * omega
+    print('Total Power:  {0:.2f} kW'.format(power*10**-3))
+
+    plot.show()
